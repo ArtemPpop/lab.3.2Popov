42940dbb895911bc4bfe2bb1edbc5f8e517fdb13
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-11-05 00:14:06 +0200

    Добавьте файлы проекта.

---
+++ lab.3.2Popov/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = 10;
+while (n < 100)
+{
+    int reversNum = 0;
+    int OrigNum = n;
+
+    int num = n;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        reversNum = (reversNum * 10) + digit;
+        num /= 10;
+    }
+
+    if (OrigNum == reversNum)
+    {
+        Console.WriteLine(OrigNum);
+    }
+
+    n++;
+}
